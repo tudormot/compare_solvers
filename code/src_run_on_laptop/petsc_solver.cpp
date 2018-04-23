ba0c939769fb14687504e74e1d2fc093a9d61205
Tudor Mot <mot.tudor@yahoo.com>
2018-04-23 17:10:26 +0200

    memory preallocation now works for any submatrix distributions.fixed pardiso solver to only attempt to calculate sol rel error if a sol was provided in input file

--- code/src_run_on_laptop/petsc_solver.cpp
+++ code/src_run_on_laptop/petsc_solver.cpp
@@ -224,41 +224,42 @@ void PETSc_solver::check_petsc_solution(linear_sys &sys)
 
 PetscErrorCode PETSc_solver::mat_preallocate_mem(linear_sys& sys)
 {
-    PetscInt local_sz[2]; //local_sz[0] is number of rows local_sz[1] is number of cols
 
-    ierr = MatGetLocalSize(A,&local_sz[0],&local_sz[1]);
-    CHKERRQ(ierr);
+    PetscInt local_preallocation_recv_buffer[nlocal][2];
 
-    PetscInt local_preallocation_recv_buffer[local_sz[0]][2];
     if(sys.node_rank == 0)
     {
-        PetscInt local_sizes[sys.no_of_nodes][2];
+        PetscInt local_sizes[sys.no_of_nodes];
         PetscInt starting_rows[sys.no_of_nodes+1];
 
-        MPI_Gather(local_sz,2,MPIU_INT,local_sizes,2,MPIU_INT,0,PETSC_COMM_WORLD);
+        MPI_Gather(&nlocal,1,MPIU_INT,local_sizes,1,MPIU_INT,0,PETSC_COMM_WORLD);
 
-        PetscInt preallocation_data[sys.no_of_nodes * local_sz[0]][2]; //last few entries might not get populated, but that is ok
+        PetscInt preallocation_data[sys.mat_dim][2]; //last few entries might not get populated, but that is ok
 
         /*now we have to populate preallocation data with the data..*/
         //first set all the array to 0
-        memset(preallocation_data,0,sizeof(PetscInt)*2*sys.no_of_nodes*local_sz[0]);
+        memset(preallocation_data,0,sizeof(PetscInt)*2*sys.mat_dim);
+
         //now populate starting rows:
         starting_rows[0]=0;
         for (int i =1 ; i<sys.no_of_nodes+1; i++)
         {
-            starting_rows[i]=starting_rows[i-1]+local_sizes[i-1][0];
+            starting_rows[i]=starting_rows[i-1]+local_sizes[i-1];
         }
+        int test_dummy_temp = 0;//this is iterated everytime a matrix location in allocated, in the end test to see that its equal to no of non zeros
         //now preallocation data..
         int current_node=0;
-        int first_index_not_in_diag_block = local_sz[0];
+        int first_index_not_in_diag_block = nlocal;
         for(PetscInt i = 0; i< sys.mat_dim-1; i++)
         {
             if(i>=starting_rows[current_node+1])
             {
                 current_node++;
-                first_index_not_in_diag_block+=local_sz[0];
+                first_index_not_in_diag_block+=local_sizes[current_node];
             }
             preallocation_data[i][0]++; //the diag elem
+            test_dummy_temp++;
+
             int row_index_start=sys.col_ch[i] -1;
             int row_index_end=sys.col_ch[i+1] -1; //these two variables will store the start and end indexes of the data we want to extract from linear_sys.elem vector
 
@@ -268,32 +269,47 @@ PetscErrorCode PETSc_solver::mat_preallocate_mem(linear_sys& sys)
                 {
                     preallocation_data[i][0]++;
                     preallocation_data[sys.row_i[j]-1][0]++;
+                    test_dummy_temp=test_dummy_temp+2;
                 }
                 else
                 {
                     preallocation_data[i][1]++;
                     preallocation_data[sys.row_i[j]-1][1]++;
+                    test_dummy_temp=test_dummy_temp+2;
                 }
             }
         }
         preallocation_data[sys.mat_dim-1][0]++; //last entry of diagonal
+        test_dummy_temp++;
+
+        if(test_dummy_temp!=sys.non_diag_no*2+sys.mat_dim)
+        {
+            std::cout<<"ERR in preallocation, preallocation not working correctly..\ntestdummy_temp = "<<test_dummy_temp<<"and total no of nonzs = "<<sys.non_diag_no*2+sys.mat_dim<<'\n';
+        }
 
         //now send preallocation data to all nodes
-        MPI_Scatter(preallocation_data,local_sz[0]*2,MPIU_INT,local_preallocation_recv_buffer,local_sz[0]*2,MPIU_INT,0,PETSC_COMM_WORLD);
+        for(int i = 0;i < sys.no_of_nodes;i++)
+        {
+            starting_rows[i]=starting_rows[i]*2;
+            local_sizes[i]=local_sizes[i]*2;
+        }
+
+        MPI_Scatterv(preallocation_data,local_sizes,starting_rows,MPIU_INT,local_preallocation_recv_buffer,nlocal*2,MPIU_INT,0,PETSC_COMM_WORLD);
     }
     else
     {
-        MPI_Gather(local_sz,2,MPIU_INT,NULL,0,MPIU_INT,0,PETSC_COMM_WORLD);
-        MPI_Scatter(NULL,0,MPIU_INT,local_preallocation_recv_buffer,local_sz[0]*2,MPIU_INT,0,PETSC_COMM_WORLD);
+        MPI_Gather(&nlocal,1,MPIU_INT,NULL,0,MPIU_INT,0,PETSC_COMM_WORLD);
+        MPI_Scatterv(NULL,NULL,NULL,MPIU_INT,local_preallocation_recv_buffer,nlocal*2,MPIU_INT,0,PETSC_COMM_WORLD);
     }
     //now, we have the local preallocation information on all nodes, but it is a bit jumbled up, not how PETSc wants it, need to do some array manipulation
-    PetscInt d_nnz[local_sz[0]];
-    PetscInt o_nnz[local_sz[0]];
-    for(int i=0; i<local_sz[0]; i++)
+    PetscInt d_nnz[nlocal];
+    PetscInt o_nnz[nlocal];
+    for(int i=0; i<nlocal; i++)
     {
         d_nnz[i]=local_preallocation_recv_buffer[i][0];
         o_nnz[i]=local_preallocation_recv_buffer[i][1];
     }
+
     ierr = MatMPIAIJSetPreallocation(A,0,d_nnz,0,o_nnz);
     CHKERRQ(ierr);
     ierr = MatSetOption(A, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
