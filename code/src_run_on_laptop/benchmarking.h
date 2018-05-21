7944724ca2550c31407f6b27ffb70d5f5f834b9a
Tudor Mot <mot.tudor@yahoo.com>
2018-05-21 09:54:38 +0200

    further code tidying up

--- code/src_run_on_laptop/benchmarking.h
+++
@@ -1,9 +0,0 @@
-#ifndef BENCHMARKING_H_INCLUDED
-#define BENCHMARKING_H_INCLUDED
-
-class benchmark{
-public:
-    time_t time_solver(lin_sys_solver &solver, linear_sys &sys );
-};
-
-#endif // BENCHMARKING_H_INCLUDED
